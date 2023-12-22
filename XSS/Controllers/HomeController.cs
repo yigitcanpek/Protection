6990ad1cd0c40b88026e299973329c1e23c0e882
Yiğitcan PEKGÜZEL <yigitcanpekguzel@ticimax.com>
2023-12-22 16:58:02 +0300

    Reflected Attack.

--- XSS/Controllers/HomeController.cs
+++ XSS/Controllers/HomeController.cs
@@ -17,6 +17,21 @@ namespace XSS.Controllers
         {
             return View();
         }
+        public IActionResult CommentAdd()
+        {
+            HttpContext.Response.Cookies.Append("email", "fcakiroglu@outlook.com");
+            HttpContext.Response.Cookies.Append("password", "1234");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CommentAdd(string name,string comment)
+        {
+
+            ViewBag.Name = name;
+            ViewBag.Comment = comment;
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
