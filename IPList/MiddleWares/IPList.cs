3ca12b89b951adcce9a0e2b3204185bb8c691428
Yiğitcan PEKGÜZEL <yigitcanpekguzel@ticimax.com>
2023-12-22 15:36:28 +0300

    Add project files.

---
+++ IPList/MiddleWares/IPList.cs
@@ -0,0 +1,9 @@
+﻿namespace IPList.MiddleWares
+{
+    public class IPList
+    {
+        public string[] WhiteList { get; set; }
+        public string[] BlackList { get; set; }
+
+    }
+}
---
+++ IPList/MiddleWares/IPSafeMiddleWare.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace IPList.MiddleWares
+{
+    public class IPSafeMiddleWare
+    {
+        private readonly RequestDelegate _delegate;
+        private readonly IPList _ipList;
+        public IPSafeMiddleWare(IOptions<IPList> ipList, RequestDelegate @delegate)
+        {
+            _ipList = ipList.Value;
+            _delegate = @delegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            IPAddress reqipAdress = context.Connection.RemoteIpAddress;
+            bool isWhiteList = _ipList.WhiteList.Where(x => IPAddress.Parse(x).Equals(reqipAdress)).Any();
+            if (!isWhiteList)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+            await _delegate(context); 
+        } 
+
+    }
+}
